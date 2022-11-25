b2fe8a53f41347e7f77f73f547ef72e02474d994
Sirin Puenggun <sirin_29@hotmail.co.th>
2022-11-25 20:58:05 +0700

    Checking Minus sign and Check devide by 0 error

--- nessesary/fraction.py
+++ nessesary/fraction.py
@@ -1,8 +1,37 @@
 class Fraction:
     def __init__(self, numer, denom):
-        self.numer = numer
-        self.denom = denom
+        if numer < 0 and denom < 0:
+            numer = -numer
+            denom = -denom
+        if denom < 0:
+            numer = -numer
+            denom = -denom
+        if denom == 0:
+            raise ValueError("Denominator shouldn't be zero.")
+        self.__numer = numer
+        self.__denom = denom
         self.floating = numer/denom
+
+    @property
+    def numer(self):
+        return self.__numer
+
+    @property
+    def denom(self):
+        return self.__denom
+    
+    @numer.setter
+    def numer(self, value):
+        self.__numer = value
+
+    @denom.setter
+    def denom(self, value):
+        if denom < 0:
+            numer = -numer
+            denom = -denom
+        if denom == 0:
+            raise ValueError("Denominator shouldn't be zero.")
+        self.__denom = value
 
     def __add__(self, other):
         """
@@ -62,4 +91,19 @@ class Fraction:
         return result
 
     def __str__(self) -> str:
+        """
+        Print Fraction Form.
+        >>> m1 = Fraction(1, 2)
+        >>> print(m1)
+        1/2
+        >>> m2 = Fraction(5, -2)
+        >>> print(m2)
+        -5/2
+        >>> m3 = Fraction(-5, 2)
+        >>> print(m3)
+        -5/2
+        >>> m4 = Fraction(-5, -2)
+        >>> print(m4)
+        5/2
+        """
         return f"{self.numer}/{self.denom}"
